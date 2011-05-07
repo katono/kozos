e99525a65c0c9bcff3d4ac339ff1063a04f3bf2f
katono <katono123@gmail.com>
2011-05-07 22:07:07 +0900

    リスト8.23まで。

--- os/main.c
+++ os/main.c
@@ -1,35 +1,14 @@
 #include "defines.h"
-#include "intr.h"
+#include "kozos.h"
 #include "interrupt.h"
-#include "serial.h"
 #include "lib.h"
-#include "dram.h"
 
-static void intr(softvec_type_t type, unsigned long sp)
+/* システム・タスクとユーザ・スレッドの起動 */
+static int start_threads(int argc, char *argv[])
 {
-	int c;
-	static char buf[32];
-	static int len;
-
-	c = getc();
-
-	if (c != '\n') {
-		buf[len++] = c;
-	} else {
-		buf[len++] = '\0';
-		if (!strncmp(buf, "echo", 4)) {
-			printf("%s\n", &buf[4]);
-		} else if (!strcmp(buf, "exit")) {
-		} else if (!strcmp(buf, "ramchk")) {
-			dram_check();
-		} else {
-			puts("unknown.\n");
-		}
-		puts("> ");
-		len = 0;
-	}
+	kz_run(test08_1_main, "command", 0x100, 0, NULL);
+	return 0;
 }
-
 
 int main(void)
 {
@@ -37,16 +16,9 @@ int main(void)
 
 	printf("kozos boot succeed!\n");
 
-	softvec_setintr(SOFTVEC_TYPE_SERINTR, intr);
-	serial_intr_recv_enable(SERIAL_DEFAULT_DEVICE);
-
-	puts("> ");
-
-	INTR_ENABLE;
-
-	while (1) {
-		asm volatile("sleep");
-	}
+	/* OSの動作開始 */
+	kz_start(start_threads, "start", 0x100, 0, NULL);
+	/* ここには戻ってこない */
 
 	return 0;
 }
